d26c8e0ed1fd80aaf99b132b1ef4a6d5105f47b2
mima <Michaela.Mattes@t-systems.com>
2022-08-02 12:42:00 +0200

    #16 #17

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "registry.terraform.io/hashicorp/azurerm"
-      version = ">=3.2"
+      version = ">=3.15"
     }
   }
   required_version = "~>1.0"
