1e4f6bdd3830d3bc27d0751931a758323c6f6e84
mima <Michaela.Mattes@t-systems.com>
2022-08-05 12:31:39 +0200

    fix issue to combine multiple rules_engine actions

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,13 @@
+output "frontdoor_firewall_policy" {
+  description = "azurerm_frontdoor_firewall_policy results"
+  value = {
+    for firewall_policy in keys(azurerm_frontdoor_firewall_policy.frontdoor_firewall_policy) :
+    firewall_policy => {
+      id = azurerm_frontdoor_firewall_policy.frontdoor_firewall_policy[firewall_policy].id
+    }
+  }
+}
+
 output "frontdoor" {
   description = "azurerm_frontdoor results"
   value = {
@@ -10,12 +20,24 @@ output "frontdoor" {
   }
 }
 
-output "frontdoor_firewall_policy" {
-  description = "azurerm_frontdoor_firewall_policy results"
+output "frontdoor_custom_https_configuration" {
+  description = "azurerm_frontdoor_custom_https_configuration results"
   value = {
-    for firewall_policy in keys(azurerm_frontdoor_firewall_policy.frontdoor_firewall_policy) :
-    firewall_policy => {
-      id = azurerm_frontdoor_firewall_policy.frontdoor_firewall_policy[firewall_policy].id
+    for frontdoor_custom_https_configuration in keys(azurerm_frontdoor_custom_https_configuration.frontdoor_custom_https_configuration) :
+    frontdoor_custom_https_configuration => {
+      id                   = azurerm_frontdoor_custom_https_configuration.frontdoor_custom_https_configuration[frontdoor_custom_https_configuration].id
+      frontend_endpoint_id = azurerm_frontdoor_custom_https_configuration.frontdoor_custom_https_configuration[frontdoor_custom_https_configuration].frontend_endpoint_id
+    }
+  }
+}
+
+output "frontdoor_rules_engine" {
+  description = "azurerm_frontdoor_rules_engine results"
+  value = {
+    for frontdoor_rules_engine in keys(var.frontdoor_rules_engine) :
+    frontdoor_rules_engine => {
+      id   = contains(local.frontdoor_rules_engine_action.override, frontdoor_rules_engine) == true ? azurerm_resource_group_template_deployment.frontdoor_rules_engine[frontdoor_rules_engine].id : azurerm_frontdoor_rules_engine.frontdoor_rules_engine[frontdoor_rules_engine].id
+      name = contains(local.frontdoor_rules_engine_action.override, frontdoor_rules_engine) == true ? azurerm_resource_group_template_deployment.frontdoor_rules_engine[frontdoor_rules_engine].name : azurerm_frontdoor_rules_engine.frontdoor_rules_engine[frontdoor_rules_engine].name
     }
   }
 }
